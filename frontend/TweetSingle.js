2ed7ab0b227e971f2dc88cdd4d161ed8dff5a888
Michael Pavelchuk <error2268@gmail.com>
2019-04-24 15:23:04 +0300

    Update TweetSingle.js

--- frontend/TweetSingle.js
+++ frontend/TweetSingle.js
@@ -12,8 +12,8 @@ function TweetSingle({
         <div className='container'>
             <div className="row card blue-grey darken-1">
                 <img src={photo} className='circle responsive-img col s2' />
-                <strong className="col s2">{fullName}</strong>
-                <span className="col s2">{userId}</span>
+                <strong className="col s3">{fullName}</strong>
+                <span className="col s1">{userId}</span>
                 <span className="col s4">{(new Date(tweetDate)).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</span>
                 <p className="col s8 white-text">{text}</p>
                 <img className="responsive-img" src={imgUrl} />
